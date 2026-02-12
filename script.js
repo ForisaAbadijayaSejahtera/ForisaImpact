54b68b713a3d99f8587f7ff9183ca60294033ee5
ForisaAbadijayaSejahtera <info.ptfias@gmail.com>
2026-02-12 11:08:54 +0700

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("waForm").addEventListener("submit",function(e){
-e.preventDefault();
-const pesan = "Halo Forisa Impact, saya ingin konsultasi.";
-window.open("https://wa.me/6281225047599?text="+encodeURIComponent(pesan));
-});
