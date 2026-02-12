a39ab3e5b4ba11e5b3bc1839f7e0e09dacbd3281
ForisaAbadijayaSejahtera <info.ptfias@gmail.com>
2026-02-12 11:22:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-document.getElementById("waForm").addEventListener("submit",function(e){
-e.preventDefault();
-const pesan = "Halo Forisa Impact, saya ingin konsultasi.";
-window.open("https://wa.me/6281225047599?text="+encodeURIComponent(pesan));
+document.getElementById("btnKonsultasi").addEventListener("click", function () {
+  const phone = "6281225057599"; // ganti nomor WA lo
+  const message = encodeURIComponent(
+    "Halo Forisa, saya ingin menjadwalkan konsultasi awal."
+  );
+  window.open(
+    `https://wa.me/${phone}?text=${message}`,
+    "_blank"
+  );
 });
