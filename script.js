1280a1b99a66751c18ec4deb220e8204c08322b4
ForisaAbadijayaSejahtera <info.ptfias@gmail.com>
2026-02-12 11:54:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
+
+  // ACCORDION
+  const headers = document.querySelectorAll(".accordion-header");
+
+  headers.forEach(header => {
+    header.addEventListener("click", function () {
+      const content = this.nextElementSibling;
+      content.classList.toggle("active");
+    });
+  });
+
+  // WHATSAPP
   const btn = document.getElementById("btnKonsultasi");
 
-  if (!btn) return;
+  if (btn) {
+    btn.addEventListener("click", function () {
+      const phone = "6281225047599";
+      const message = encodeURIComponent(
+        "Halo Forisa, saya ingin menjadwalkan konsultasi awal."
+      );
 
-  btn.addEventListener("click", function () {
-    const phone = "6281225047599";
-    const message = encodeURIComponent(
-      "Halo Forisa, saya ingin menjadwalkan konsultasi awal."
-    );
+      window.open(
+        `https://wa.me/${phone}?text=${message}`,
+        "_blank"
+      );
+    });
+  }
 
-    window.open(
-      `https://wa.me/${phone}?text=${message}`,
-      "_blank"
-    );
-  });
 });
